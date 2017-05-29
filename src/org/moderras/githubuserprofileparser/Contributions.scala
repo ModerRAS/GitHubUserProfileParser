9eea07493ab8c81fa3ea6bc14904dffc56c6cd5e
ModerRAS <adqewsx@gmail.com>
2017-05-29 11:23:48 +0800

    just back up

--- src/org/moderras/githubuserprofileparser/Contributions.scala
+++ src/org/moderras/githubuserprofileparser/Contributions.scala
@@ -1,3 +1,5 @@
+package org.moderras.githubuserprofileparser
+
 /**
   * Created on 2017/5/29.
   *
@@ -6,6 +8,10 @@
 class Contributions(date:String, count:String) {
   val Date:String = date
   val Count:String = count
+  private val DateNums:Array[Int] = date.split("-").map(_.toInt)
+  val Year:Int = DateNums(0)
+  val Month:Int = DateNums(1)
+  val Day:Int = DateNums(2)
 
   override def toString: String = "Date="+Date+"\t"+"Count="+Count
 }
