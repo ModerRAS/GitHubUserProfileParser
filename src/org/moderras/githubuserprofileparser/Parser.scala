6ff96e3ea904b277e107a87cd70aff10bfb75338
ModerRAS <adqewsx@gmail.com>
2017-06-01 20:49:46 +0800

    Try to use tree to create xml document

--- src/org/moderras/githubuserprofileparser/Parser.scala
+++ src/org/moderras/githubuserprofileparser/Parser.scala
@@ -5,7 +5,7 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 import scala.collection.mutable.ListBuffer
-import scala.xml.XML
+import scala.xml.{Elem, XML}
 
 /**
   * Created on 2017/5/29.
@@ -15,6 +15,60 @@ import scala.xml.XML
 
 
 object Parser {
+  def readXML(string: String): Set[String] = XML.load(string).child.map(_.attribute("Name").toString).toSet
+
+  def writeXML(filename: String, nodes: Map[String, List[Contributions]]): Unit = {
+    /**
+      * 输入数据应该是一个cont的list的map,和一个文件名
+      * map的键是用户名,每个用户名对应的是其活跃相关
+      * 写的xml格式
+      * <person name ="">
+      * <year attr="2017" total="1000">
+      * <month attr="1" total="100">
+      * <day attr="1" contributions="10" />
+      * </month>
+      * </year>
+      * </person>
+      * 写一下关于递归调用的版本?
+      * 想一下怎么写吧...
+      *
+      */
+    def translate(nodes: Map[String, List[Contributions]]) = {
+      val NodesIterator = nodes.iterator
+
+      def conventToTree(map: Map[String, List[Contributions]]) = {
+
+      }
+
+      def addone(list: List[Contributions]): Elem = {
+
+        def addYear(contributions: Contributions) = {
+
+        }
+
+        def addMonth(contributions: Contributions) = {
+
+        }
+
+        def addDay(contributions: Contributions) = {
+
+        }
+      }
+
+
+    }
+
+    def toWrite() = {
+
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val ret = getLinks("https://github.com/moderras")
+    for (i <- ret) println(i.toString)
+  }
+
   def getLinks(url:String): List[Contributions] = {
     val doc:Document = Jsoup.connect(url).get()
     val links:Elements = doc.select("rect")
@@ -25,16 +79,5 @@ object Parser {
       ret += new Contributions(ne.attr("data-date"),ne.attr("data-count"))
     }
     ret.toList
-  }
-
-  def readXML(string: String):Set[String] = XML.load(string).child.map(_.attribute("Name").toString).toSet
-  def writeXML(string: String, list: List[String]): Unit = {
-
-  }
-
-
-  def main(args: Array[String]): Unit = {
-    val ret = getLinks("https://github.com/moderras")
-    for(i<-ret) println(i.toString)
   }
 }
---
+++ src/org/moderras/githubuserprofileparser/TreeNodes.scala
@@ -0,0 +1,18 @@
+package org.moderras.githubuserprofileparser
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created on 2017/6/1.
+  *
+  * @author ModerRAS
+  */
+class TreeNodes(dad: TreeNodes, inname: String, inattr: String) {
+  val father: TreeNodes = dad
+  val name: String = inname
+  val attr: String = inattr
+  val total: Int =
+  private var children: ListBuffer[TreeNodes] = new ListBuffer[TreeNodes]
+
+  def +(treeNodes: TreeNodes): Unit = children += treeNodes
+}
