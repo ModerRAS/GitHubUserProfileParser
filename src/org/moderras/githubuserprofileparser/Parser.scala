43c1e3bcbfade04f689e57e284ed7a335b10a668
ModerRAS <adqewsx@gmail.com>
2017-06-01 20:49:46 +0800

    Try to use tree to create xml document
    Signed-off-by: ModerRAS <adqewsx@gmail.com>

--- src/org/moderras/githubuserprofileparser/Parser.scala
+++ src/org/moderras/githubuserprofileparser/Parser.scala
@@ -15,6 +15,61 @@ import scala.xml.XML
 
 
 object Parser {
+  def readXML(string: String): Set[String] = XML.load(string).child.map(_.attribute("Name").toString).toSet
+
+  def writeXML(filename: String, nodes: Map[String, List[Contributions]]): Unit = {
+    /**
+      * 输入数据应该是一个cont的list的map,和一个文件名
+      * map的键是用户名,每个用户名对应的是其活跃相关
+      * 写的xml格式
+      * <person name ="">
+      * <year attr="2017" total="1000">
+      * <month attr="1" total="100">
+      * <day attr="1" contributions="10" />
+      * </month>
+      * </year>
+      * </person>
+      * 写一下关于递归调用的版本?
+      * 想一下怎么写吧...
+      * 用最粗暴的方式:把所有的cont遍历一遍之后根据年份扔进一个list里,然后把这个年份作为key建立一个map
+      * 然后里面再遍历所有的月份扔进一个list里,同理月份作为key建立一个map,
+      */
+    def translate(nodes: Map[String, List[Contributions]]) = {
+      val NodesIterator = nodes.iterator
+
+      def convent(inputs: List[Contributions]) = {
+        var map: Map[Int, List[Contributions]] = Map()
+        var list = inputs
+        var maphead = list.head
+
+        def fi1(a: Contributions): Boolean = maphead.Year == a.Year
+
+        def nfi1(a: Contributions): Boolean = maphead.Year != a.Year
+
+        //val list2 = list.filter((_) => maphead.Year == _.Year)//类型擦除了......
+        while (list.exists(nfi1)) {
+          val list2 = list.filter(fi1)
+          list = list.filter(nfi1)
+        }
+
+
+
+      }
+
+
+    }
+
+    def toWrite() = {
+
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val ret = getLinks("https://github.com/moderras")
+    for (i <- ret) println(i.toString)
+  }
+
   def getLinks(url:String): List[Contributions] = {
     val doc:Document = Jsoup.connect(url).get()
     val links:Elements = doc.select("rect")
@@ -25,16 +80,5 @@ object Parser {
       ret += new Contributions(ne.attr("data-date"),ne.attr("data-count"))
     }
     ret.toList
-  }
-
-  def readXML(string: String):Set[String] = XML.load(string).child.map(_.attribute("Name").toString).toSet
-  def writeXML(string: String, list: List[String]): Unit = {
-
-  }
-
-
-  def main(args: Array[String]): Unit = {
-    val ret = getLinks("https://github.com/moderras")
-    for(i<-ret) println(i.toString)
   }
 }
