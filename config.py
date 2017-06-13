7336c3d3e0ed562043d80cfff4a1621cc491dd0e
ModerRAS <adqewsx@gmail.com>
2017-06-13 11:39:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 person = [
     "ModerRAS"
-]
+]  # Write GitHub user name
+date_time = [
+    [2017],
+    [2017, 6]
+]  # Write [year] or [year,month]
+
+parser = False # If True , it will flush all data and do parser from GitHub
+print_on_cmd = True  # If True , it will print what to be write on the file
+save_on_the_file = True  # If True , it will save the data on the file
+file = "output.txt"  # File path
