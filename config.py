ff8d000c542dfb02adaf905f89841992ea17f522
ModerRAS <adqewsx@gmail.com>
2017-06-13 18:06:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,5 @@ parser = True  # If True , it will flush all data and do parser from GitHub
 print_on_cmd = True  # If True , it will print what to be write on the file
 save_on_the_file = True  # If True , it will save the data on the file
 file = "output.md"  # File path
-depends_on_week = False
 get_last_week = True # If True, it will just get last week everyone's contributions
 use_markdown = False
