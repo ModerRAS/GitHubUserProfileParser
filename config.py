18168f754444c44935c4667220359979485c01af
ModerRAS <adqewsx@gmail.com>
2017-06-13 18:02:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,12 @@ person = [
 date_time = [
     [2017],
     [2017, 6]
-]  # Write [year] or [year,month]
+]  # Write [year] or [year,month] , if get_last_week is True , it will no use
 
-parser = False # If True , it will flush all data and do parser from GitHub
+parser = True  # If True , it will flush all data and do parser from GitHub
 print_on_cmd = True  # If True , it will print what to be write on the file
 save_on_the_file = True  # If True , it will save the data on the file
-file = "output.txt"  # File path
+file = "output.md"  # File path
+depends_on_week = False
+get_last_week = True # If True, it will just get last week everyone's contributions
+use_markdown = False
